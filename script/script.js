edf6a8fedb625a27da313b9ff5887571055800c1
Fatima-YF <f.ucf.eva@gmail.com>
2017-04-09 23:40:40 +0200

    map

---
+++ script/script.js
@@ -0,0 +1,65 @@
+
+function clickZone1(){
+	document.getElementById("1").classList.add('animation');
+}
+function zone_1() {
+	document.getElementById("Carte_Oran").src = "../img/zone1.png";
+}
+function clickZone2(){
+	document.getElementById("2").classList.add('animation');
+}
+
+function zone_2() {
+	document.getElementById("Carte_Oran").src = "../img/zone2.png";
+}
+function clickZone3(){
+	document.getElementById("3").classList.add('animation');
+}
+
+function zone_3() {
+	document.getElementById("Carte_Oran").src = "../img/zone3.png";
+}
+function clickZone4(){
+	document.getElementById("4").classList.add('animation');
+}
+function zone_4() {
+	document.getElementById("Carte_Oran").src = "../img/zone4.png";
+}
+function clickZone5 (){
+	document.getElementById("5").classList.add('animation');
+}
+function zone_5() {
+	document.getElementById("Carte_Oran").src = "../img/zone5.png";
+}
+function clickZone6 (){
+	document.getElementById("6").classList.add('animation');
+}
+function zone_6() {
+	document.getElementById("Carte_Oran").src = "../img/zone6.png";
+}
+
+function clickZone7 (){
+	document.getElementById("7").classList.add('animation');
+}
+function zone_7() {
+	document.getElementById("Carte_Oran").src = "../img/zone7.png";
+}
+
+function clickZone8 (){
+	document.getElementById("8").classList.add('animation');
+}
+
+function zone_8() {
+	document.getElementById("Carte_Oran").src = "../img/zone8.png";
+}
+
+function clickZone9 (){
+	document.getElementById("9").classList.add('animation');
+}
+function zone_9() {
+	document.getElementById("Carte_Oran").src = "../img/zone9.png";
+}
+
+function carte_oran() {
+    document.getElementById("Carte_Oran").src = "../img/Oran.png";
+}
